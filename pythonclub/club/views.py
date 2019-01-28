c645a61fb13b5bc1a9f9c5a1d687a441f87440d0
John Nguyen <johnhvn94@gmail.com>
2019-01-28 11:53:05 -0800

    Added resource view

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import TopicType
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+def posttypes (request):
+    type_list=TopicType.objects.all()
+    return render (request, 'club/types.html', {'type_list': type_list})
